43330c8a29dae6c377e52ec3e17eb9f07ae97991
hongquang2335 <quangdauhong342@gmail.com>
2026-04-10 11:03:50 +0700

    quick fix

--- app/src/main/java/com/example/fanzone/ui/screens/EventDetailScreen.kt
+++ app/src/main/java/com/example/fanzone/ui/screens/EventDetailScreen.kt
@@ -3,6 +3,7 @@ package com.example.fanzone.ui.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
